f6a17dfef3ad045dc8e708a03b1dfcdf1506f5a2
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2019-08-06 17:28:34 -0400

    Create vis_pdb_v1.py

---
+++ vis_pdb_v1.py
@@ -0,0 +1,44 @@
+renee <complicated eastern european name> - DARPA project manager for PREPARE
+
+orthologs  - how do reads align show up in data vs known orthologs?
+distribution of ortholog expression across 
+
+tweaking with alignment params
+
+cytoscape - cluego
+XGSA
+inter-species gene ontology comparison.
+
+REMEMBER - genes can belong to multiple pathways. 
+what might be found that there is some similarity, but also som edifference
+how to reconcile truth something as precise as genes, to less precise 
+concept pathway. 
+
+#######
+
+be aware of redundancy 
+levels of counts b/w orthologs 
+consider something similar in the pathway analysis; 
+might say what is the distribution of correct pathways identified. 
+
+relationship between pathways? network-based analysis b/w pathways?
+
+layers of specificity when doing GO 
+
+##transcription
+
+cellular process
+biological function
+organism 
+
+hierarchical system.
+
+######
+
+same playbook of genes used for classification -> IPA pathway enrichment analyses
+
+replace angie and adam's names if they both die
+
+ferret flu is most physiologically accurate except for possibly non-human primates
+
+--
